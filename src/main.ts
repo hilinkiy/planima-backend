b5a5fa6cbd14bfb013b87bbfae6bea9521c0e122
Behzod <bexzod6061@gmail.com>
2024-11-14 15:53:44 +0500

    fixed bugs

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-
-	app.setGlobalPrefix('api')
+	const port = process.env.PORT || 3000
 	app.use(cookieParser())
-	app.enableCors({
-		origin: ['http://localhost:3000', 'http://localhost:3001'],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
-
-	await app.listen(4201)
+	await app.listen(port)
 }
 bootstrap()
